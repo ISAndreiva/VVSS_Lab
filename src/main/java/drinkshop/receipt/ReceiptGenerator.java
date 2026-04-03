9aedec1cafb75fe959f65df0cf8241067c3d96a0
Stefy1209 <stefangramada09@gmail.com>
2026-04-03 13:44:57 +0300

    method updated

--- src/main/java/drinkshop/receipt/ReceiptGenerator.java
+++ src/main/java/drinkshop/receipt/ReceiptGenerator.java
@@ -4,7 +4,9 @@ import drinkshop.domain.Order;
 import drinkshop.domain.OrderItem;
 import drinkshop.domain.Product;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ReceiptGenerator {
     private ReceiptGenerator() {
@@ -14,12 +16,28 @@ public class ReceiptGenerator {
 
 
     public static String generate(Order o, List<Product> products) {
+        if(o == null) {
+            throw new NullPointerException();
+        }
+
+        if(products == null) {
+            throw new NullPointerException();
+        }
+
         StringBuilder sb = new StringBuilder();
         sb.append("===== BON FISCAL =====\n").append("Comanda #").append(o.getId()).append("\n");
+
+        Map<Integer, Product> mapProducts = new HashMap<Integer, Product>();
+
+        for(Product product : products) {
+            mapProducts.put(product.getId(), product);
+        }
+
         for (OrderItem i : o.getItems()) {
-            Product p = products.stream().filter(p1->i.getProduct().getId()==p1.getId()).toList().get(0);
+            Product p = mapProducts.get(i.getProduct().getId());
             sb.append(p.getNume()).append(": ").append(p.getPret()).append(" x ").append(i.getQuantity()).append(" = ").append(i.getTotal()).append(" RON\n");
         }
+        
         sb.append("---------------------\nTOTAL: ").append(o.getTotalPrice()).append(" RON\n=====================\n");
         return sb.toString();
     }
